9dda67f8e9a1e43d9b6230179d1042a4699b321e
URIELdc227352 <dc22735@um.edu.mo>
2026-03-06 20:44:18 +0800

    Add files via upload

---
+++ CISC3003-Practice06-2026MAR06/public/js/functions.js
@@ -0,0 +1,28 @@
+/* define functions here */
+function calculateTotal(quantity,price){
+	return price* quantity;
+
+}
+function calculateTax(subtotal, rate) {
+    return subtotal * rate;
+}
+function calculateShipping(subtotal, threshold) {
+    return (subtotal > threshold) ? 0 : 40;
+}
+function calculateGrandTotal(subtotal, tax, shipping) {
+    return subtotal + tax + shipping;
+}
+function outputCurrency(num) {
+    document.write("$" + num.toFixed(2));
+}
+
+function outputCartRow(file, title, quantity, price, total) {
+    document.write("<tr>");
+    document.write("<td><img src='images/" + file + "'></td>");
+    document.write("<td>" + title + "</td>");
+    document.write("<td>" + quantity + "</td>");
+    document.write("<td>"); outputCurrency(price); document.write("</td>");
+    document.write("<td>"); outputCurrency(total); document.write("</td>");
+    document.write("</tr>");
+}
+        
